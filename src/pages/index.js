390b4c8f07fe411ae48ca80062d1fc1c08b6f9ab
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-08-19 17:41:53 -0700

    Added testimonial slider to homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import GravityForm from "../components/gravity-form/gravity-form"
 import SEO from "../components/seo"
 import { GetHomePageMeta } from "../hooks/getHomePageMeta"
 import { graphql } from "gatsby"
+import Slider from "../components/slider/slider"
 
 import aetna from "../images/aetna.jpg"
 import blueShield from "../images/blue.jpg"
@@ -26,6 +27,7 @@ const IndexPage = ({ data }) => {
     pages: { nodes },
   } = GetHomePageMeta()
   const { seo, date } = nodes[0]
+
   return (
     <Layout>
       <SEO seoInfo={seo} siteUrl={siteUrl} siteName={siteName} date={date} />
@@ -42,12 +44,12 @@ const IndexPage = ({ data }) => {
               <h1>Find out if you have peripheral artery disease</h1>
               <p>
                 Peripheral artery disease (PAD) is when your arteries become
-                narrowed or blocked, usually due to plaque or calcium buildup.
-                This is a normal part of aging and primarily impacts the
-                arteries in the legs. When blood vessels become blocked, painful
-                wounds or ulcers can develop. At Valley Radiology, our highly
-                trained vascular and interventional radiologists use their
-                expertise and knowledge to treat PAD successfully.
+                clogged, usually due to plaque or calcium buildup. This is a
+                normal part of aging and primarily affects the arteries in the
+                legs. When blood vessels are blocked, painful wounds or ulcers
+                can develop. Our highly trained vascular and interventional
+                specialists at Valley Radiology use their knowledge and
+                expertise to treat PAD successfully.
               </p>
             </Col>
             <Col lg={6} className={homePageStyles.heroForm}>
@@ -70,7 +72,7 @@ const IndexPage = ({ data }) => {
       <Container fluid className={homePageStyles.callOut}>
         <Container>
           If left untreated, PAD could result in amputation, stroke, and even
-          death
+          death!
         </Container>
       </Container>
       <Container fluid>
@@ -112,9 +114,9 @@ const IndexPage = ({ data }) => {
           <Row>
             <Col lg={6} className={homePageStyles.verticalCenter}>
               <Img
-                className={homePageStyles.homeImg}
+                className={`${homePageStyles.homeImg} ${homePageStyles.blueBorder}`}
                 fluid={data.section3.childImageSharp.fluid}
-                alt="A couple hiking outdoors."
+                alt="A couple sitting together."
               />
             </Col>
             <Col lg={6} className={homePageStyles.verticalCenter}>
@@ -122,8 +124,8 @@ const IndexPage = ({ data }) => {
               <p>
                 Not everyone experiences the same symptoms of PAD. This
                 condition is often misdiagnosed, making it vital to pay
-                attention to risk factors. The most significant risk factors are{" "}
-                <strong>hypertension, diabetes, and smoking</strong>. If you’re
+                attention to risk factors. The most significant risk factors are
+                <strong> hypertension, diabetes, and smoking.</strong> If you’re
                 under 50 and overweight or have high blood pressure, we
                 recommend a checkup and screening. If you’re over 65 and/or have
                 leg pain, numbness, or other symptoms, make an appointment today
@@ -150,10 +152,11 @@ const IndexPage = ({ data }) => {
               <p>
                 The experienced specialists at Valley Radiology regularly
                 diagnose and treat men and women with PAD. Our team will
-                carefully diagnose your condition within state-of-the-art
-                facilities and deliver a tailored treatment plan for best
-                results. We also encourage and oversee lifestyle changes to
-                reduce pain and enhance your quality of life.
+                carefully diagnose and treat your condition utilizing the latest
+                technology and equipment. We create a tailored treatment plan to
+                deliver the best results for our patients. We also encourage and
+                oversee lifestyle changes to reduce pain and enhance your
+                quality of life.
               </p>
               <p className={homePageStyles.buttonBottom}>
                 <Button
@@ -175,14 +178,58 @@ const IndexPage = ({ data }) => {
           </Row>
         </Container>
       </Container>
-      <Container fluid className={homePageStyles.genericSection}>
-        <div className={"text-center"}>
-          <Img
-            className={`${homePageStyles.homeImg} ${homePageStyles.noBorder}`}
-            fluid={data.sliderImage.childImageSharp.fluid}
-            alt="Testimonial slider."
-          />
-        </div>
+      <Container fluid>
+        <Container
+          className={`${homePageStyles.genericSection} ${homePageStyles.testimonialSlider}`}
+        >
+          <div className={"text-center"}>
+            <Slider>
+              <p>
+                <em>
+                  "I have used this facility for multiple types of exams. I can
+                  not say enough good things about this center. From the
+                  friendly front desk staff to techs that have always been so
+                  sweet and comforting to how fast I get my results...most of
+                  the time it is the same day!! I highly recommend this center
+                  to any of my friends and family. I do not go any where else
+                  for these services."
+                </em>
+                <br />
+                <div className={homePageStyles.author}>John S.</div>
+              </p>
+              <p>
+                <em>
+                  "I have always received a exceptional care when I visit Valley
+                  Radiology. Everyone I have dealt with is very friendly, yet
+                  very professional. The phone is always answered when I call.
+                  The wait time for tests is very minimal. The technicians
+                  always explain in detail what to do and what they are doing.
+                  And your privacy is also a main concern. I will not go
+                  anywhere else!"
+                </em>
+                <br />
+                <div className={homePageStyles.author}>Amy B.</div>
+              </p>
+              <p>
+                <em>
+                  "Valley Radiology, in Angier, probably has the best customer
+                  service of any place I have ever walked into. From start to
+                  finish, the staff was wonderful. I was called the day before
+                  my appointment, and the lady was delightful and very
+                  personable. The day of my appointment, I was greeted at my car
+                  by another friendly staff member, and each person that I came
+                  in contact with at that facility was so courteous and kind.
+                  They were caring and interested in my history and life. No one
+                  rushed. They were right on time. I didn't have to wait. I
+                  don't normally write reviews, but I was impressed. Thank you
+                  to all the staff at Valley Radiology! Great job!"
+                </em>
+                <br />
+                <div className={homePageStyles.author}>Jake L.</div>
+              </p>
+            </Slider>
+          </div>
+        </Container>
       </Container>
       <Container fluid className={homePageStyles.lightBack}>
         <Container className={homePageStyles.genericSection}>
@@ -208,12 +255,13 @@ const IndexPage = ({ data }) => {
               <p>
                 Our board-certified vascular and interventional specialists,
                 Murali Meka, MD, and Bryan Swilley, MD, specialize in diagnosing
-                and treating PAD. They each bring a strong focus and background
-                in effective therapy through interventional radiology. Having
-                this expertise in a single location, focused on you, means that
-                you’re receiving the best treatment plan discussed among the
-                team. Regardless of your diagnosis, we can help determine the
-                next steps and provide a treatment option that works for you.
+                and treating PAD. They each bring a strong focus and extensive
+                training in effective therapy through interventional radiology.
+                Having this expertise in a single location, focused on you,
+                means that you’re receiving the most innovative treatment
+                methods possible. Regardless of your diagnosis, we can help
+                determine the next steps and provide a treatment option that
+                works for you.
               </p>
               <p className={homePageStyles.buttonBottom}>
                 <Button
@@ -286,10 +334,10 @@ const IndexPage = ({ data }) => {
               <h2>Quality Care Close to Home</h2>
               <p>
                 Receive the specialized care you deserve at Valley Radiology.
-                Our Fayetteville Clinic offers diagnosis and treatment for men
-                and women with peripheral artery disease and is now accepting
-                new patients. Schedule your appointment to discuss your
-                individualized treatment options.
+                Our Fayetteville based Valley Radiology clinic offers diagnosis
+                and treatment for men and women with peripheral artery disease
+                and is now accepting new patients. Schedule your appointment to
+                discuss your individualized treatment plan now.
               </p>
               <p className={homePageStyles.buttonBottom}>
                 <Button
